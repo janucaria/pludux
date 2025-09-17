275881ff1becd0ce68dfa73690da5295736d76f3
Januar Andaria <janucaria@gmail.com>
2025-09-18 02:20:22 +0800

    backtest summary and trade_session

--- apps/backtest/lib/sources/backtest.cxx
+++ apps/backtest/lib/sources/backtest.cxx
@@ -20,6 +20,8 @@ export module pludux.backtest;
 
 export import pludux.backtest.asset;
 export import pludux.backtest.profile;
+export import pludux.backtest.trade_session;
+export import pludux.backtest.backtest_summary;
 
 export namespace pludux {
 
